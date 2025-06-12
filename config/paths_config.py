25839e8b73c03c4597aebb0572e2c146a89404c7
Kishan bhati <bhatikaran634@gmail.com>
2025-06-12 19:28:52 +0530

    comet model training

--- config/paths_config.py
+++ config/paths_config.py
@@ -25,3 +25,14 @@ ANIME2ANIME_ENCODED = os.path.join(PROCESSED_DIR, "anime2anime_encoded.pkl")
 
 USER2USER_DECODED = os.path.join(PROCESSED_DIR, "user2user_decoded.pkl")
 ANIME2ANIME_DECODED = os.path.join(PROCESSED_DIR, "anime2anime_decoded.pkl")
+
+
+
+###################### MODEL TRAINING #######################333
+
+MODEL_DIR = "artifacts/model"
+WEIGHTS_DIR = "artifacts/weights"
+MODEL_PATH = os.path.join(MODEL_DIR,"model.h5")
+ANIME_WEIGHTS_PATH = os.path.join(WEIGHTS_DIR,"anime_weights.pkl")
+USER_WEIGHTS_PATH = os.path.join(WEIGHTS_DIR,"user_weights.pkl")
+CHECKPOINT_FILE_PATH = "artifacts/model_checkpoint/weights.weights.h5"
